613eab5d680f2fc5fa7339787ecf8c2aaac1b349
Wangouf <wanghso@foxmail.com>
2022-08-07 15:10:21 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,25 +1,50 @@
+#--------DEPENDECE-------
+#sudo apt-get install git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig git python
+
+
+
+#-------CONFIGURATION-----
+
+src_dir=..
 config=vendor/lito-perf_defconfig
 arch=arm64
-#export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-if [ ! -d "./aarch64-linux-android-4.9" ];
+
+
+#-------TOOLCHAIN-----------
+if [ ! -d "./toolchains" ];
 then
-  echo "downloading compiler---------------------"
-  git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b ndk-release-r22
+git clone https://github.com/Shubhamvis98/toolchains.git
 fi
-export CROSS_COMPILE=$(pwd)/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+export PATH=$(pwd)/toolchains/clang-r428724/bin:$(pwd)/toolchains/aarch64-linux-android-4.9/bin:$(pwd)/arm-linux-androideabi-4.9/bin:$PATH
+
+export CROSS_COMPILE=aarch64-linux-android-
+
+export CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+export CLANG_TRIPLE=aarch64-linux-gnu-
+
 export ARCH=$arch
-git clone https://github.com/Edward-Projects/android_kernel_lenovo_J607Z.git
-sudo apt-get install -y build-essential kernel-package libncurses5-dev bzip2
-cd ./android_kernel_lenovo_J607Z
+#-----------RUN-----------
+
+cd $src_dir
 if [ -d "./out" ];
 then
   echo "cleaning......"
   rm -rf ./out
-  make mrproper
-  
+  make mrproper 
 fi
-  echo "configing--------------------------------"
-  make O=out $config
+  echo "configing----------------------------"
+  make O=out ARCH=$arch $config
   echo "build start--------------------------------"
-  make O=out -j$(nproc)
+  make -j3 CC=clang O=out
+  
+  
+  
+  
+
+#git clone https://http://mirrors.ustc.edu.cn/aosp/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b ndk-release-r21
+
+#git clone https://github.91chi.fun/https://github.com/bigsaltyfishes/kernel_lenovo_J607Z.git
+
 
